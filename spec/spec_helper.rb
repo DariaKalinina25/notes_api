4e728d4c38fb67dca5afe678d72ce2b9ef9d7f01
DariaKalinina25 <meuw.meuw20@gmail.com>
2025-06-11 05:06:53 +0500

    chore(test): enhance RSpec config with color, documentation format, and seed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 RSpec.configure do |config|
+  config.formatter = :documentation
+  config.color = true
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -8,4 +11,7 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.order = :random
+  Kernel.srand config.seed
 end
